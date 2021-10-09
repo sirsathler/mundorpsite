253f1dce9207093406a865ce76eb7d4aaf0664ec
RivaldoCM <rivaldocardoso2010@hotmail.com>
2021-10-08 22:42:20 -0300

    botao de prev e netx colocados

--- src/components/Pages/Pageshop/Pageshop.jsx
+++ src/components/Pages/Pageshop/Pageshop.jsx
@@ -1,4 +1,5 @@
 import css from './Pageshop.css'
+import Pagination from './Pagination/Pagination'
 import Banner from './Shopbanner/Banner.jsx'
 import Shopcards from './Shopcards/Shopcards.jsx'
 import Shopfilter from './Shopfilter/Shopfilter.jsx'
@@ -11,6 +12,7 @@ export default function Pageshop(){
             <div className="container">
                 <Shopfilter />
                 <Shopcards />
+                <Pagination />
             </div>
             <Shoppacks sectionSelect='premium' title='assine o premium!'/>
             <Shoppacks title='mundopoints'/>
