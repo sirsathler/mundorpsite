ec08567144da674ca20284263179c6fd2543c92a
RivaldoCM <rivaldocardoso2010@hotmail.com>
2021-11-03 00:36:29 -0300

    adicionando components à page product

--- src/components/Pages/Pageproduct/Pageproduct.jsx
+++ src/components/Pages/Pageproduct/Pageproduct.jsx
@@ -3,24 +3,21 @@ import Carousel from 'react-elastic-carousel'
 import Card from '../Pageshop/Shopcards/Card.jsx'
 import Productcolor from './Productcolor.jsx'
 import Purchase from '../../Global/Buttons/Purchase'
+import Banner from '../../Global/Banner/Banner'
 
 import image from '../../../images/shop/cars/DTS.png'
 import mp from '../../../images/shop/icons/mp.png'
+import Vehicleinfo from '../../Global/Vehicleinfo'
 
 
 export default function Pageproduct(){
     return(
         <section className="page-product">
-            <div className="product-banner">
-                <div className="container">
-                    <div className="product-name"><p>dts</p></div>
-                    <div className="product-image">
-                        <img src={image} alt="" />
-                    </div>
-                </div>
-            </div>
+            <Banner />
             <div className="product-infos">
-
+                <Vehicleinfo isPageVehicle={true}/>
+                <Vehicleinfo isPageVehicle={true}/>
+                <Vehicleinfo isPageVehicle={true}/>
             </div>
             <div className="product-sets">
                 <div className="container">
@@ -41,11 +38,13 @@ export default function Pageproduct(){
                     <div className="content-button">
                         <Purchase />
                     </div>
+                    <div className="cashback"><p>você poderá pedir reembolso em ate x horas spós a confirmação do pagamento</p></div>
                 </div>
             </div>
             <div className="products">
+                <p>confira também!</p>
                 <div className="container">
-                    <Carousel itemsToShow={3} pagination={false}>
+                    <Carousel itemsToShow={3} pagination={false} >
                         <Card />
                         <Card />
                         <Card />
