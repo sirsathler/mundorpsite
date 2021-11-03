1b2fa5dff8d6adbc2750ba50f68a898455e7ab48
RivaldoCM <rivaldocardoso2010@hotmail.com>
2021-11-03 19:16:59 -0300

    adicionado configs no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,50 @@
 import css from './Footer.css'
 import imageDisc from '../../images/social/discord.png'
+import imageFace from '../../images/social/facebook.png'
+import imageInsta from '../../images/social/insta.png'
 
 export default function Footer(){
     return(
-        <a href="">
-            <footer>
-                <img src={imageDisc} alt="" />
-                <p>acesse nosso discord!</p>
-            </footer>
-        </a>
+        <footer>
+            <div className="container">
+                <div className="footer-top">
+                    <div className="footer-title">
+                        <h2>mundo</h2>
+                    </div>
+                    <div className="social-media">
+                        <a href=""><img src={imageDisc} alt="" /></a>
+                        <a href=""><img src={imageFace} alt="" /></a>
+                        <a href=""><img src={imageInsta} alt="" /></a>
+                    </div>
+                </div>
+                <div className="footer-center">
+                    <div className="links-site">
+                        <h3>informações</h3>
+                        <div className="footer-target">
+                            <a href=""><p>meu perfil</p></a>
+                            <a href=""><p>como jogar</p></a>
+                            <a href=""><p>loja oficial</p></a>
+                        </div>
+                    </div>
+                    <div className="comunication">
+                        <h3>suporte</h3>
+                        <div className="footer-target">
+                            <a href=""><p>faq</p></a>
+                            <a href=""><p>feedback</p></a>
+                            <a href=""><p>fale conosco</p></a>
+                        </div>
+                    </div>
+                    <div className="about-us">
+                        <h3>A equipe</h3>
+                        <div className="footer-target">
+                            <a href=""><p>membros</p></a>
+                            <a href=""><p>participar</p></a>
+                            <a href=""><p>o projeto</p></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-bottom"></div>
+            </div>
+        </footer>
     )
 }
