21d146f2aeee0e8be3622bc950e20d999f042897
RivaldoCM <rivaldocardoso2010@hotmail.com>
2021-11-11 18:13:53 -0300

    Bugs corrigidos e page finlizada.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import css from './App.css'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-import Header from './components/Header/Header.jsx'
 import Pageprofile from './components/Pages/Pageprofile/Pageprofile.jsx'
 import Pageshop from './components/Pages/Pageshop/Pageshop.jsx'
 import Pageproduct from './components/Pages/Pageproduct/Pageproduct.jsx'
 import Footer from './components/Footer/Footer'
-import Headerstick from './components/Header/Headerstick'
 import Pagevehicle from './components/Pages/Pagevehicle/Pagevehicle'
 import Pagelogin from './components/Pages/Pagelogin/Pagelogin'
 
@@ -14,8 +12,15 @@ export default function App(){
     return(
         <Router>
             <div className="App">
-                <Pagelogin path="/login" component={Pagelogin}/>
-
+                <Switch>
+                    <Route path="/" exact/>
+                    <Route path="/player" component={Pageprofile}/>
+                    <Route path="/shop" component={Pageshop}/>
+                    <Route path="/product" component={Pageproduct}/>
+                    <Route path="/vehicle" component={Pagevehicle}/>
+                    <Pagelogin path="/login" component={Pagelogin}/>
+                </Switch>
+                <Footer />
             </div>
         </Router>
     )
