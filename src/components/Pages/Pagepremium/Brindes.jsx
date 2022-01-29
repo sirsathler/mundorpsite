74f8ae2f52d047ff5c693966c5d0d2babfad92d6
Guilherme Faglioni <larafaglioni@gmail.com>
2022-01-28 22:30:51 -0300

    finalizando section brindes

--- src/components/Pages/Pagepremium/Brindes.jsx
+++ src/components/Pages/Pagepremium/Brindes.jsx
@@ -1,7 +1,17 @@
 import css from './Brindes.css'
+import image from '../../../images/premiumshop.png'
 
 export default function Brindes(){
     return(
-        <div></div>
-    )
+       <section className= 'rewards-container'>
+            <div className= 'text-wrapper'>
+                    <p className= 'title'>BRINDES GRATUITOS</p>
+                    <p className= 'subtitle'>COM A ASSINATURA PREMIUM VOCÊ RECEBE BÔNUS DE
+                    MP E DIVERSOS OUTROS BRINDES TODA SEMANA!</p>
+            </div>
+            <div className= 'img-wrapper'>
+                <img src={image}/>
+            </div>              
+       </section>
+)   
 }
