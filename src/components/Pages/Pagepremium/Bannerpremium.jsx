87f86263d3dcb4f42024e4dcedc7a78a97276e01
RivaldoCM <rivaldocardoso2010@hotmail.com>
2022-01-29 02:31:26 -0300

    adiconado section com cards-premium e juntei todos os códigos do pessoal em um só, deixando os bugs e problemas para resolver com eles futuramente

--- src/components/Pages/Pagepremium/Bannerpremium.jsx
+++
@@ -1,7 +0,0 @@
-import css from './Bannerpremium.css'
-
-export default function Bannerpremium(){
-    return(
-        <div></div>
-    )
-}
--- src/components/Pages/Pagepremium/Pagepremium.jsx
+++ src/components/Pages/Pagepremium/Pagepremium.jsx
@@ -1,16 +1,73 @@
 import css from './Pagepremium.css'
+import Card from '../Pageshop/Shopcards/Card'
 
-import Bannerpremium from './Bannerpremium'
-import Brindes from './Brindes'
-import Vantagens from './Vantagens'
+import image from '../../../images/premiumshop.png'
+
+const premium = [
+    {
+        "id": 1,
+        "description": "1 mês de assinatura premium",
+        "image": "https://imgur.com/yOgB7ZF.png",
+        "mp": 100,
+        "rp": 150
+    },
+    {
+        "id": 2,
+        "description": "3 mês de assinatura premium",
+        "image": "https://imgur.com/Ev7XYYj.png",
+        "mp": 100,
+        "rp": 150
+    },
+    {
+        "id": 3,
+        "description": "6 mês de assinatura premium",
+        "image": "https://imgur.com/SEXXmQi.png",
+        "mp": 100,
+        "rp": 150
+    },
+]
+/*NAO SEI SE CONSIGO REPLICAR COM UMA JÁ EXISTENTE, VOU OLHAR HOJE DE MANHA 
+29/01*/ 
 
 export default function Pagepremium(){
     return(
         <div className= "page-premium">
-             <Bannerpremium />
-             <Brindes />
-             <Vantagens />
+            {/*GABY FAZ O SEU AQUI EM CIMA*/}
+            <section className= 'rewards-container'>
+                <div className= 'text-wrapper'>
+                    <p className= 'title'>BRINDES GRATUITOS</p>
+                    <p className= 'subtitle'>
+                        COM A ASSINATURA PREMIUM VOCÊ RECEBE BÔNUS DE
+                        MP E DIVERSOS OUTROS BRINDES TODA SEMANA!
+                    </p>
+                </div>
+                <div className= 'img-wrapper'>
+                    <img src={image}/>
+                </div>              
+            </section>
+            <div>
+                <section className='text'>
+                    <p className='title'>MAIS VAGAS NA GARAGEM! </p>
+                    <p className='subtitle'>VOCÊ PODERÁ GUARDAR MAIS CARROS EM SUA GARAGEM PESSOAL!</p>
+                </section>
+
+                <section className='background'>
+                    <p className='orange-text title'>E MUITAS OUTRAS VANTAGENS!</p>
+                    <p className='subtitle'>ASSINE JÁ O MUNDO PREMIUIM PARA TER ACESSO A ESSAS E DIVERSAS OUTRAS VANTAGENS EM NOSSOS SERVIDORES!</p>
+                </section>
+            </div>
+            <section className='cards-premium'>
+                <div className='cards-premium-title'>
+                    <h2>assine o premium!</h2>
+                </div>
+                <div className='cards-premium-container'>
+                    {
+                        premium.map((card)=>(
+                            <Card image={card.image} name={card.description} price={card.mp}/>
+                        ))
+                    }
+                </div>
+            </section>
         </div>
- 
     )
 }
