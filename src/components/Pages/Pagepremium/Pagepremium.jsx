a886b97edfc30481f767bba958f3012ae9fcf8ae
SuurSahin <andreraatz@hotmail.com>
2022-01-28 21:45:29 -0300

    Sprint Seção mais vagas & Seção outras vantagens terminada

--- src/components/Pages/Pagepremium/Pagepremium.jsx
+++ src/components/Pages/Pagepremium/Pagepremium.jsx
@@ -1,9 +1,18 @@
 import css from './Pagepremium.css'
 
 import Bannerpremium from './Bannerpremium'
+import Brindes from './Brindes'
+import Vantagens from './Vantagens'
 
 export default function Pagepremium(){
     return(
-        <Bannerpremium />
+        <div className='page-premium'>
+
+            <Bannerpremium />
+            <Brindes />
+            <Vantagens />
+
+        </div>
+
     )
 }
