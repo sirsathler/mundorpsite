461aeddefdad4ff29f23589d5133aaa6967e0ca6
ArthurTvr <arthurdanilotavares@hotmail.com>
2022-02-11 19:36:46 -0300

    Finalizando a section armazém

--- src/components/Pages/Pagestorage/Pagestorage.jsx
+++ src/components/Pages/Pagestorage/Pagestorage.jsx
@@ -1,5 +1,14 @@
-export default function Pagestorage(){
-    return(
-        <div></div>
+import css from './Pagestorage.css'
+export default function Pagestorage() {
+    return (
+        <div>
+            <section className="section-buy-itens">
+                <div className='section-text'>
+                    <p>Você não possui itens no armazém compre algo na <a href='#' id='shop'>LOJA!</a></p>
+                   
+                </div>
+                    <p id='text-botton'>Retire o item acessando o celular dentro do jogo!</p>
+            </section>
+        </div>
     )
 }
