a150d2a4631383cbf038ab8adde05a956ea29f5e
RivaldoCM <rivaldocardoso2010@hotmail.com>
2022-02-10 21:07:31 -0300

    adicionei mais configs ao modal

--- src/components/Global/Modals/Modal.jsx
+++ src/components/Global/Modals/Modal.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import css from "./Modal.css"
+import img from "../../../images/icons/mp.png"
+
+import Purchase from "../Buttons/Purchase"
 
 export default function Modal(props){
 
@@ -15,7 +18,29 @@ export default function Modal(props){
                     <div className="product-image">
                         <img src={props.img} alt="" />
                     </div>
-                    <div className="product-details"></div>
+                    <div className="product-details">
+                        <div className="box-details">
+                            <div className="confirm-shopping">
+                                <div className="icon-type-shopping">
+                                    <img src={img} alt="" />
+                                </div>
+                                <div className="box-calc">
+                                    <p>Saldo atual :</p>
+                                    <p>5.000</p>
+                                </div>
+                                <div className="box-calc">
+                                    <p>RTX 3090 Ti :</p>
+                                    <p>6.000</p>
+                                </div>
+                                <div className="border"></div>{/*gambiarra?*/}
+                                <div className="box-calc">
+                                    <p>Saldo Total :</p>
+                                    <p>-1.000</p>
+                                </div>
+                            </div>
+                            <Purchase text="confirmar compra"/>
+                        </div>
+                    </div>
                 </div>
                 <p>Você não vai pedir reembolso fdp</p>
             </div>
