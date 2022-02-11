999d610f5e2d1787fa9d8991f8161c6c198a88e5
Marcus uwu <viniciusdahora2013@gmail.com>
2022-02-11 20:08:33 -0300

    Finalizando as sections banne e UM NOVO JEITO DE JOGAR UNTURNED

--- src/components/Pages/Pageabout/Pageabout.jsx
+++ src/components/Pages/Pageabout/Pageabout.jsx
@@ -1,5 +1,15 @@
-export default function Pageabout(){
-    return(
-        <div></div>
+import css from './Pageabout.css'
+
+export default function Pageabout() {
+    return (
+        <div>
+            <section className='about-banner-container'>
+            </section>
+            <section className='about-un-novo-jeito-de-jogar'>
+                <p className='texto'>UM NOVO JEITO DE JOGAR UNTURNED</p>
+                <p className='texto2'>TRABALHE, GANHE DINHEIRO, COMPRE CARROS, MOTOS, CASAS E MUITO MAIS!</p>
+            </section>
+
+        </div>
     )
 }
