80f84f02a281faa8f98491ddecf91b3dafca4771
Gaby Vas <vasgaby9@gmail.com>
2022-02-14 16:00:24 -0300

    Fiz a parte do "Seu armazém de itens" completa

--- src/components/Pages/Pagestorage/Pagestorage.jsx
+++ src/components/Pages/Pagestorage/Pagestorage.jsx
@@ -1,5 +1,42 @@
+import css from "./Pagestorage.css"
 export default function Pagestorage(){
+
     return(
-        <div></div>
-    )
-}
+        <div>
+            <section className="storage-header">
+
+                <div className="storage-title">                    
+
+                <p>Seu armazém de itens</p>
+
+                </div>
+
+                <div className="storage-info-container">
+
+                    <div className="storage-wrapper">
+
+                        <p className="storage-value">5</p>
+                        <p>Itens no armazém</p>
+
+                    </div>
+
+                    <div className="storage-wrapper">
+
+                        <p className="storage-value">20</p>
+                        <p>Capacidade máxima</p>
+
+                    </div>
+
+                    <div className="storage-wrapper">
+
+                        <p className="storage-value">68000MP</p>
+                        <p>Valor de armazém</p>
+                        
+                    </div>
+
+                </div>
+
+            </section>
+
+        </div>
+    )}
