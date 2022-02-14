319410a37166dba70f61c158bfd2a62958bb5040
Marcus uwu <viniciusdahora2013@gmail.com>
2022-02-14 16:35:51 -0300

    resolvendo responsivo

--- src/components/Pages/Pageabout/Pageabout.jsx
+++ src/components/Pages/Pageabout/Pageabout.jsx
@@ -3,11 +3,13 @@ import css from './Pageabout.css'
 export default function Pageabout() {
     return (
         <div>
-            <section className='about-banner-container'>
+            <section className='about-banner'>
             </section>
-            <section className='about-un-novo-jeito-de-jogar'>
-                <p className='texto'>UM NOVO JEITO DE JOGAR UNTURNED</p>
-                <p className='texto2'>TRABALHE, GANHE DINHEIRO, COMPRE CARROS, MOTOS, CASAS E MUITO MAIS!</p>
+            <section className='about-info'>
+                <div className='about-text-wrapper'>
+                    <p className='about-text'>UM NOVO JEITO DE JOGAR UNTURNED</p>
+                    <p className='about-subtext'>TRABALHE, GANHE DINHEIRO, COMPRE CARROS, MOTOS, CASAS E MUITO MAIS!</p>
+                </div>
             </section>
 
         </div>
