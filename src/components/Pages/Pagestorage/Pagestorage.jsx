47f08a6c6f2af53fc5b0d3f8d5e014d79f69353c
ArthurTvr <arthurdanilotavares@hotmail.com>
2022-02-14 19:37:30 -0300

    Finalizando a branch page/storage

--- src/components/Pages/Pagestorage/Pagestorage.jsx
+++ src/components/Pages/Pagestorage/Pagestorage.jsx
@@ -4,48 +4,33 @@ export default function Pagestorage() {
     return (
         <div>
             <section className="storage-header">
-
                 <div className="storage-title">
-
                     <p>Seu armazém de itens</p>
-
                 </div>
-
                 <div className="storage-info-container">
-
                     <div className="storage-wrapper">
-
-                        <p className="storage-value">5</p>
-                        <p>Itens no armazém</p>
-
+                        <p className="highlight">5</p>
+                        <p className="description">Itens no armazém</p>
                     </div>
-
                     <div className="storage-wrapper">
-
-                        <p className="storage-value">20</p>
-                        <p>Capacidade máxima</p>
-
+                        <p className="highlight">20</p>
+                        <p className="description">Capacidade máxima</p>
                     </div>
-
                     <div className="storage-wrapper">
-
-                        <p className="storage-value">68000MP</p>
-                        <p>Valor de armazém</p>
-
+                        <p className="highlight">68000MP</p>
+                        <p className="description">Valor de armazém</p>
                     </div>
-
                 </div>
-
-            </section>
-            
-            <section className="section-buy-itens">
-                <div className='section-text'>
-                    <p>Você não possui itens no armazém compre algo na <a href='#' id='shop'>LOJA!</a></p>
-
-                </div>
-                <p id='text-botton'>Retire o item acessando o celular dentro do jogo!</p>
             </section>
 
+            <section className="section-storage">
+                <div>
+                    <div className='section-text'>
+                        <p>Você não possui itens no armazém compre algo na <a href='/shop' className="highlight">LOJA!</a></p>
+                    </div>
+                </div>
+                <p className="description text-bottom" >Retire o item acessando o celular dentro do jogo!</p>
+            </section>
         </div>
     )
 }
