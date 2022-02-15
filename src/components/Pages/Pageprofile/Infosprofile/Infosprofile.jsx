d69411df2b5505e61848ba0c2a54731617b62f6d
Guilherme Faglioni <larafaglioni@gmail.com>
2022-02-14 21:27:55 -0300

    conclui section de perfil da page/profile

--- src/components/Pages/Pageprofile/Infosprofile/Infosprofile.jsx
+++ src/components/Pages/Pageprofile/Infosprofile/Infosprofile.jsx
@@ -3,16 +3,29 @@ import Aboutplayer from './Aboutplayer/Aboutplayer.jsx'
 
 function Infosprofile(){
     return(
-        <section className="infos-profile">
-             <div className="photo-profile">
-                <div className="photo-player">
-                    <div className="photo-content"></div>
+        <section className="section-profile">
+            <div className="profile-wallpaper"></div>
+            <div className="profile-info">
+                <div className="profile-text-wrapper">
+                    <h1 className="profile-text">NOME</h1>
+                    <h2 className="profile-text">CARGO</h2>
                 </div>
+                <div className="user-info">
+                    <div className="info-wrapper">
+                        <p className="highlight">000</p>
+                        <p className="description">ROLEPOINTS</p>
+                    </div>
+                    <div className="info-wrapper">
+                        <p className="highlight">000</p>
+                        <p className="description">MUNDO POINTS</p>
+                    </div>
+                    <div className="info-wrapper">
+                        <p className="highlight">#00000</p>
+                        <p className="description">PLAYER ID</p>
+                    </div>
+                </div>
+                    
              </div>
-             <div className="bg-profile">
-
-            </div>
-            <Aboutplayer />
         </section>
     )
 }
