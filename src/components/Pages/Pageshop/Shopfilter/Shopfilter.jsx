6009690571bdf7b979fe876d8eb10c381e5cc0b2
Matheus Sathler <mattsathler@gmail.com>
2022-02-18 13:18:59 -0300

    WIP

--- src/components/Pages/Pageshop/Shopfilter/Shopfilter.jsx
+++ src/components/Pages/Pageshop/Shopfilter/Shopfilter.jsx
@@ -3,73 +3,53 @@ import css from './Shopfilter.css'
 import Typebutton from './Typebutton.jsx'
 import Valuefilter from './Valuefilter.jsx'
 
-let iconList = [
-    {
-        "id": 1,
-        "name": "home",
-        "image": "https://imgur.com/iXgrWOm.png"
-    },
-    {
-        "id": 2,
-        "name": "car",
-        "image": "https://imgur.com/lg3HAUQ.png"
-    },
-    {
-        "id": 3,
-        "name": "clots",
-        "image": "https://imgur.com/lwbHP7j.png"
-    },
-    {
-        "id": 4,
-        "name": "furniture",
-        "image": "https://imgur.com/sKrKfcz.png"
-    },
-    {
-        "id": 5,
-        "name": "guns",
-        "image": "https://imgur.com/zHoE6UM.png"
-    }
-]
-
 let nameFilter = [
     {
         "id": 1,
-        "name" : "premium",
+        "name": "premium",
         "cssId": "premium"
     },
     {
         "id": 2,
-        "name" : "preço Min",
+        "name": "preço Min",
         "cssId": "price-min"
     },
     {
         "id": 3,
-        "name" : "preço Max",
+        "name": "preço Max",
         "cssId": "price-max"
     },
     {
         "id": 4,
-        "name" : "moeda",
+        "name": "moeda",
         "cssId": "currency"
     }
 ]
 
-export default function Shopfilter(){
-    return(
+export default function Shopfilter() {
+
+    return (
         <section className='shop-filter'>
             <div className="filter-type">
-                {
-                    iconList.map((icon)=>(
-                        <Typebutton key={icon.id} id={icon.name} image={icon.image}/>
-                    ))
-                }
+                <div className="type-content">
+                    <img src={require('../../../../images/shop/icons/buildings.png').default} alt="" />
+                </div>
+                <div className="type-content">
+                    <img src={require('../../../../images/shop/icons/buildings.png').default} alt="" />
+                </div>
+                <div className="type-content">
+                    <img src={require('../../../../images/shop/icons/buildings.png').default} alt="" />
+                </div>
+                <div className="type-content">
+                    <img src={require('../../../../images/shop/icons/buildings.png').default} alt="" />
+                </div>
+                <div className="type-content">
+                    <img src={require('../../../../images/shop/icons/buildings.png').default} alt="" />
+                </div>
             </div>
             <div className="filter-value">
-                {
-                    nameFilter.map((type)=>(
-                        <Valuefilter key={type.id} name={type.name} cssId={type.cssId}/>
-                    ))
-                }
+                <div className='value-item'>
+                </div>
             </div>
             <div className="filter-search-bar">
                 <Searchbar />
