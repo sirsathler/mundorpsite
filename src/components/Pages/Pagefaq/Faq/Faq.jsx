458261e27d6a117969712cae7f2b55eb5be43bbf
Guilherme Faglioni <larafaglioni@gmail.com>
2022-02-22 22:05:52 -0300

    finalizando e corrigindo page/faq

--- src/components/Pages/Pagefaq/Faq/Faq.jsx
+++ src/components/Pages/Pagefaq/Faq/Faq.jsx
@@ -1,12 +1,11 @@
 import css from './Faq.css'
 
 export default function Faq(props){ 
-    console.log(props.title)
     return(
         <section className='faq-container'>
             <div className="faq-inner">
                 <p className='faq-title highlight'>{props.title}</p>
-                <p className='faq-subtitle'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam, ducimus architecto nesciunt quod veritatis sint quam in aliquid, sed dolorem. Omnis iste praesentium unde, at error dolor architecto repellat.</p>
+                <p className='faq-subtitle'>{props.description}</p>
             </div>
         </section>
     )
--- src/components/Pages/Pagefaq/Pagefaq.jsx
+++ src/components/Pages/Pagefaq/Pagefaq.jsx
@@ -10,13 +10,13 @@ export default function Pagefaq(){
             </section>
             <div>
                 <Faq title="titulo1" description="description1"/>
-                <Faq/>
-                <Faq/>
-                <Faq/>
-                <Faq/>
-                <Faq/>
-                <Faq/>
-                <Faq/>
+                <Faq title="titulo2" description="description2"/>
+                <Faq title="titulo3" description="description3"/>
+                <Faq title="titulo4" description="description4"/>
+                <Faq title="titulo5" description="description5"/>
+                <Faq title="titulo6" description="description6"/>
+                <Faq title="titulo7" description="description7"/>
+                <Faq title="titulo8" description="description8"/>
             </div>
         </div>
     )
