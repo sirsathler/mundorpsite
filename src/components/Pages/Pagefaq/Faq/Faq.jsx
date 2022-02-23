ca0779c158b243d97181a361033b278466260b8a
Guilherme Faglioni <larafaglioni@gmail.com>
2022-02-22 21:16:50 -0300

    finalizando page/faq

--- src/components/Pages/Pagefaq/Faq/Faq.jsx
+++ src/components/Pages/Pagefaq/Faq/Faq.jsx
@@ -4,7 +4,7 @@ export default function Faq(){
     return(
         <section className='faq-container'>
             <p className='faq-title highlight'>TÍTULO</p>
-            <p className='faq-subtitle description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam, ducimus architecto nesciunt quod veritatis sint quam in aliquid, sed dolorem. Omnis iste praesentium unde, at error dolor architecto repellat.</p>
+            <p className='faq-subtitle'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam, ducimus architecto nesciunt quod veritatis sint quam in aliquid, sed dolorem. Omnis iste praesentium unde, at error dolor architecto repellat.</p>
         </section>
     )
 }
--- src/components/Pages/Pagefaq/Pagefaq.jsx
+++ src/components/Pages/Pagefaq/Pagefaq.jsx
@@ -3,12 +3,19 @@ import css from './Pagefaq.css'
 
 export default function Pagefaq(){
     return(
-        <div className='container'>
+        <div>
             <section className='faq-header-container'>
                 <p className='faq-header-title highlight'>FAQ</p>
                 <p className='faq-header-subtitle description'>LEIA AS PERGUNTAS MAIS FREQUENTES DE NOSSA COMUNIDADE!</p>
             </section>
             <div>
+                <Faq/>
+                <Faq/>
+                <Faq/>
+                <Faq/>
+                <Faq/>
+                <Faq/>
+                <Faq/>
                 <Faq/>
             </div>
         </div>
