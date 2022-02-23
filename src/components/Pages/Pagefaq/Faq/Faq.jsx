7feedc54d6f42d7a56ac8d3254a7c50afa7a48fe
Matheus Sathler <mattsathler@gmail.com>
2022-02-22 21:31:16 -0300

    Fixing FAQ sections

--- src/components/Pages/Pagefaq/Faq/Faq.jsx
+++ src/components/Pages/Pagefaq/Faq/Faq.jsx
@@ -3,8 +3,10 @@ import css from './Faq.css'
 export default function Faq(){
     return(
         <section className='faq-container'>
-            <p className='faq-title highlight'>TÍTULO</p>
-            <p className='faq-subtitle'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam, ducimus architecto nesciunt quod veritatis sint quam in aliquid, sed dolorem. Omnis iste praesentium unde, at error dolor architecto repellat.</p>
+            <div className="faq-inner">
+                <p className='faq-title highlight'>TÍTULO</p>
+                <p className='faq-subtitle'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam, ducimus architecto nesciunt quod veritatis sint quam in aliquid, sed dolorem. Omnis iste praesentium unde, at error dolor architecto repellat.</p>
+            </div>
         </section>
     )
 }
