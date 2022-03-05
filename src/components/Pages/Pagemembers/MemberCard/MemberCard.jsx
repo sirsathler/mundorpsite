a77adf785323c639e8930ee645758a6c2337fffc
ArthurTvr <arthurdanilotavares@hotmail.com>
2022-03-04 22:32:55 -0300

    WIP

--- src/components/Pages/Pagemembers/MemberCard/MemberCard.jsx
+++ src/components/Pages/Pagemembers/MemberCard/MemberCard.jsx
@@ -1,13 +1,14 @@
+import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers'
 import css from './MemberCard.scss'
-export default function MemberCard() {
+export default function MemberCard(props) {
     return (
         <div className='member-card'>
             <div className='member-card__image'>
-               
+                <img src={props.image} alt="member-image" /> 
             </div>
-            <p className='member-card__info member-card__info--name'>Matheus Sathler</p>
-            <p className='member-card__info member-card__info--description'>UM EXÍMIO GAROTO DE PROGRAMA</p>
-            <p className='member-card__info member-card__info--job'>DONO DO PROJETO</p>
+            <p className='member-card__info member-card__info--name'>{props.name}</p>
+            <p className='member-card__info member-card__info--description'>{props.description}</p>
+            <p className='member-card__info member-card__info--job'>{props.job}</p>
         </div>
     )
 }
--- src/components/Pages/Pagemembers/Pagemembers.jsx
+++ src/components/Pages/Pagemembers/Pagemembers.jsx
@@ -1,26 +1,40 @@
-import css from './Pagemembers.css'
+import css from './Pagemembers.scss'
 import MemberCard from './MemberCard/MemberCard'
 
 export default function Pagemembers() {
     return (
-
-        <div>
-            <section className='members-info'>
-                <div className='members-text-wrapper'>
-
+        <div className='page-members'>
+            <section className='project-owners'>
+                <div className='page-members__title page-members__title--main'>
                     <p className='members-text highlight'>NOSSO TIME</p>
                     <p className='members-subtext'>CONHEÇA AQUELES RESPONSÁVEIS PELO PROJETO ESTAR NO AR!</p>
                 </div>
+                <div className='cards-container'>
+                    <div className='cards-wrapper'>
+                        <MemberCard name="Matheus" description="UM EXÍMIO GAROTO DE PROGRAMA" job="DONO DO PROJETO" image="https://imgur.com/btE03lj.png" />
+                        <MemberCard name="Rivas" description="NÃO VIRE PROGRAMADOR" job="LÍDER DE FRONT-END" image="https://imgur.com/WG97vq0.png" />
+                        <MemberCard name="Luiz Fernando" description="Água Dura Pedra Mole tanto fura até que bate" job="LÍDER DE BACK-END" image="https://imgur.com/rCVBwEF.png" />
+                    </div>
+                </div>
             </section>
-            <section className=''>
-                <MemberCard></MemberCard>
-
+            <section className='project-employees'>
+                <div className='page-members__title'>
+                    <p className='members-text highlight'>DEVS</p>
+                </div>
+                <div className='cards-container'>
+                    <div className='cards-wrapper'>
+                        <MemberCard name="Marcus Vinicius " description="VOCÊ NÃO É CSS MAS É PURO ESTILO" job="desenvolverdor Front-End" image="https://imgur.com/3nHLNG5.png" />
+                        <MemberCard name="Débora Toledo" description="PALMEIRAS CONTINUA SEM MUNDIAL" job="desenvolvedor Front-end" image="https://imgur.com/beFx37D.png" />
+                        <MemberCard name="Pedro Marabisa" description="MALANDRO É MALANDRO , MANÉ É MANÉ" job="desenvolverdor Back-end" image="https://imgur.com/VE2oFMy.png" />
+                        <MemberCard name="Guilherme Faglioni" description="NO MUNDO SE SEPARA OS SERES EM DOIS TIPOS:OS AVESTRUZES E OS NÃO-AVESTRUZES" job="desenvolvedor back-end" image="https://imgur.com/ZAYDDDV.png" />
+                        <MemberCard name="Julia Ayres" description="QUALQUER MERDA AÍ" job="desenvolvedor Front-end" image="https://imgur.com/bIw9srs.png" />
+                        <MemberCard name="Arthur Tavares" description="MALANDRO NÃO PARA, MALANDRO DÁ UM TEMPO" job="desenvolvedor Front-end" image="https://imgur.com/fEJ3mDz.png" />
+                        <MemberCard name="Lara Faglioni" description="SE AMAR FOR UM CRIME, ME PRENDA POR TAL DELITO" job="desenvolvedor Front-end" image="https://imgur.com/1bR8kPL.png" />
+                        <MemberCard name="Gabrielly Vasques" description="Fé no pai que o site sai" job="desenvolvedor Front-end" image="https://imgur.com/slbSlGy.png" />
+                        <MemberCard name="Júlia Torres" description="'...'" job="MUDA" image="https://imgur.com/uzrIbRr.png" />
+                    </div>
+                </div>
             </section>
-            <section className='members-info'>
-                <p className='members-text highlight'>DEVS</p>
-            </section>
-
         </div >
-
     )
 }
